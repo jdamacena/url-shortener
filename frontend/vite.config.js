65b1e7580900a8273bef5c27654c76b7af52583b
Junior Damacena <19500428+jdamacena@users.noreply.github.com>
2025-06-16 21:03:51 -0300

    chore: update frontend dependencies and configuration
    - Removed postcss.config.js as Tailwind CSS is now integrated directly in vite.config.js.
    - Updated vite.config.js to include Tailwind CSS plugin and removed autoprefixer configuration.
    - Added .gitignore files for both frontend and backend to exclude environment files and build artifacts.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,12 @@
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import autoprefixer from "autoprefixer";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  css: {
-    postcss: {
-      plugins: [autoprefixer],
-    },
-  },
+  css: { },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
