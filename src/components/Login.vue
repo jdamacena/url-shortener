f53ab94c040f8cc039cfdffb0e60cfd3b05362a5
Junior Damacena <19500428+jdamacena@users.noreply.github.com>
2025-07-06 01:47:53 -0300

    feat: add password visibility toggle and confirm password validation to login and registration forms

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -8,7 +8,25 @@
       </div>
       <div class="mb-6">
         <label class="block mb-2 text-gray-700">Password</label>
-        <input v-model="password" type="password" required class="w-full p-3 border rounded" />
+        <div class="relative">
+          <input :type="showPassword ? 'text' : 'password'" v-model="password" required
+            class="w-full p-3 border rounded pr-10" />
+          <button type="button" @click="showPassword = !showPassword" tabindex="-1"
+            class="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 focus:outline-none">
+            <svg v-if="showPassword" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+            </svg>
+            <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.542-7a9.956 9.956 0 012.223-3.592m3.31-2.687A9.956 9.956 0 0112 5c4.478 0 8.268 2.943 9.542 7a9.965 9.965 0 01-4.293 5.411M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+            </svg>
+          </button>
+        </div>
       </div>
       <button type="submit"
         class="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold rounded-lg p-3 shadow transition">Login</button>
@@ -32,6 +50,7 @@ export default {
     const router = useRouter()
     const username = ref('')
     const password = ref('')
+    const showPassword = ref(false)
     const error = ref('')
 
     async function login() {
@@ -46,7 +65,7 @@ export default {
       }
     }
 
-    return { username, password, error, login }
+    return { username, password, showPassword, error, login }
   }
 }
 </script>
